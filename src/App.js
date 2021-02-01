36bd616f29a6baf1e434f553459f31bd92cd5814
farooq sanaullah <48545729+farooqsanaullah@users.noreply.github.com>
2021-02-01 20:15:02 +0500

    create-flex-wrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./Components/Layout";
-
+import "./styles.css";
 export default function App() {
   return <Layout />;
 }
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,35 @@
 import React from "react";
-
-
+import Box from "@material-ui/core/Box/index";
+import Card from "./Card";
 export default function Layout() {
   return (
-    <>
-    Layout
-    </>
+    <div style={{ width: "100%" }}>
+      <Box display="flex" flexWrap="wrap" p={1} bgcolor="background.paper">
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+        <Box component="div" style={{ paddingRight: 20 }}>
+          <Card />
+        </Box>
+      </Box>
+    </div>
   );
 }
